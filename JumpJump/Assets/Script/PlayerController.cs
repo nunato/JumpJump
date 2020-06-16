2f0ffe8c1efe7fbf54bcbdb7e9119e1bd79d527e
nunato <kyrie601@yahoo.co.jp>
2020-06-16 23:07:17 +0900

    足場を追加

--- JumpJump/Assets/Script/PlayerController.cs
+++ JumpJump/Assets/Script/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 	Rigidbody2D rigit2d;
+	Animator animator;
 	float jumpForce = 680.0f;
 	float walkForce = 30.0f;
 	float maxWalkSpeed = 2.0f;
@@ -12,11 +13,14 @@ public class PlayerController : MonoBehaviour
 	void Start ()
 	{
 		this.rigit2d = GetComponent<Rigidbody2D>();
+		this.animator = GetComponent<Animator>();
 	}
 	
 	void Update ()
 	{
-		if( Input.GetKeyDown( KeyCode.Space ) ){
+		if( Input.GetKeyDown( KeyCode.Space ) &&
+			this.rigit2d.velocity.y == 0 ){
+			this.animator.SetTrigger("JumpTrigger");
 			this.rigit2d.AddForce( transform.up * this.jumpForce );
 		}
 
@@ -32,6 +36,13 @@ public class PlayerController : MonoBehaviour
 
 		if( key != 0 ){
 			transform.localScale = new Vector3( key, 1, 1 );
+		}
+
+		if( this.rigit2d.velocity.y == 0 ){
+			this.animator.speed = speeddx / 2.0f;
+		}
+		else{
+			this.animator.speed = 1.0f;
 		}
 	}
 }
